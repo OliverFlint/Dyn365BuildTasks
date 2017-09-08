7eecc461ac10524ae9e2235c670588d1a63af993
Oliver Flint <oliver@9limited.co.uk>
2017-09-08 01:05:08 +0100

    Added skipping on import when solution is already installed

--- Dyn365SolutionImport/SolutionImport.ps1
+++ Dyn365SolutionImport/SolutionImport.ps1
@@ -17,12 +17,40 @@ try {
     $pub = Get-VstsInput -Name pub -AsBool
     $mwt = Get-VstsInput -Name mwt -AsInt
     $ovw = Get-VstsInput -Name ovw -AsBool
+    $skp = Get-VstsInput -Name skp -AsBool
 
     $connString = "AuthType=$auth;Domain=$dmn;Username=$usn;Password=$pwd;Url=$url"
     $connString
 
     $conn = Get-CrmConnection -ConnectionString $connString
 
+    $slnFile = Get-Item $sln
+    $root = $slnFile.Directory.FullName
+
+    #get solution version from zip
+    Expand-Archive -Path $sln -DestinationPath "$root\temp" -Force
+    [xml]$slnXml = Get-Content "$root\temp\solution.xml"
+    $slnZipVersion = $slnXml.ImportExportXml.SolutionManifest.Version
+    Write-Host "Solution version from zip file is $slnZipVersion"
+    $slnZipUniqueName = $slnXml.ImportExportXml.SolutionManifest.UniqueName
+    Write-Host "Solution name from zip file is $slnZipUniqueName"
+
+    #get solution version from Dynamics 365
+    $solutions = Get-CrmRecords -conn $conn -EntityLogicalName solution -FilterAttribute uniquename -FilterOperator eq -FilterValue $slnZipUniqueName -Fields "version"
+    if($solutions.CrmRecords.Count -gt 0)
+    {
+        $solution = $solutions.CrmRecords[0]
+        $solutionVersion = $solution.version
+        Write-Host "Solution version already installed is $solutionVersion"
+    
+        if($skp -and $slnZipVersion -eq $solutionVersion)
+        {
+            Write-Warning "Skipping! Solution versions match ($solutionVersion)"
+            return;
+        }
+    }
+
+    #import solution
     $exp = "Import-CrmSolution -conn `$conn -SolutionFilePath `$sln"
     if($act) {$exp = "$exp -ActivatePlugIns"}
     if($pub) {$exp = "$exp -PublishChanges"}
